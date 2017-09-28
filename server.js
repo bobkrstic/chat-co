0e8414de403458810642fcb998c3d816dafa801c
Slobodan Krstic <slobodankrstic@calvisitor-10-105-153-144.calvisitor.1918.berkeley.edu>
2017-09-27 20:59:30 -0700

    heroku connection bugs

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
 
+
+io.listen(port, function(){
+	console.log("Listening on io port");
+});
+
 io.on('connection', function(socket) {
 
 	console.log("Connected 1");
@@ -23,6 +28,10 @@ io.on('connection', function(socket) {
 
 // var staticAsset = require('static-asset');
 app.use(express.static(__dirname + "/") );
+
+
+
+
 
 
 http.listen(port, function(){
